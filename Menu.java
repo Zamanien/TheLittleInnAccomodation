6c52f6dd74b17ae1800a0c04bc6bbf79e4049d75
Tariq <43186550+Zamanien@users.noreply.github.com>
2018-10-23 15:43:50 +0200

    Mellemrum tilføjet

--- Menu.java
+++ Menu.java
@@ -14,9 +14,6 @@ public class Menu
 		System.out.println("|                              |");
 		System.out.println("|        Tlf: 48275562         |");
 		System.out.println("|      Email: Brown@TLN.dk     |");
-		System.out.println("|                              |");
-		System.out.println("|                              |");
-		System.out.println("|             Menu:            |");
 		System.out.println("|______________________________|");
 
 			//Konstruktør som kalder på klassen (RoomList)
@@ -31,6 +28,7 @@ public class Menu
 			System.out.println("____________________________________________");
 			System.out.println("|                                           |");
 			System.out.println("| Menu:                                     |");
+         System.out.println("|                                           |");
 			System.out.println("| -Press '1' to make a reservatin           |");
 			System.out.println("| -Press '2' to view rooms                  |");
 			System.out.println("| -Press '3' to view Customer List          |");
@@ -44,16 +42,16 @@ public class Menu
 			//if-else metoder over valg
 			if(choice.equals("1"))
 			{
-				System.out.println("Make a reservation");
+				System.out.println("\nMake a reservation");
 				
-				System.out.println("Choose a room");
+				System.out.println("Choose a room\n");
 				int roomNumber = Input.getInt();
 				roomsList.reserveRoom(roomNumber, allCustomers);
 			}
 			//Listen fra klassen RoomList kaldes hvis "2" indtastes
 			else if(choice.equals("2"))
 			{
-				System.out.println("Room List: ");
+				System.out.println("\nRoom List: \n");
 				roomsList.printRoomList();
 			}
 
@@ -64,18 +62,18 @@ public class Menu
 
 			else if(choice.equals("4"))
 			{
-				System.out.println("Please enter the room number to check out");
+				System.out.println("\nPlease enter the room number to check out\n");
 				int roomNumber = Input.getInt();
 				allCustomers.removeCustomer(roomNumber);
 				roomsList.checkOut(roomNumber);
-				System.out.println("Customer has been checked out");
+				System.out.println("\nCustomer has been checked out\n");
 			}
 			else if(choice.equals("5"))
 			{
-				System.out.println("Please enter the room number to clean");
+				System.out.println("\nPlease enter the room number to clean\n");
 				int roomNumber = Input.getInt();
             	Cleaning.cleaning(roomNumber, roomsList);
-				System.out.println("Room "+roomNumber+" has been cleaned"); 
+				System.out.println("Room "+roomNumber+" has been cleaned\n"); 
 			}
 			else if(choice.equals("6"))
 			{
